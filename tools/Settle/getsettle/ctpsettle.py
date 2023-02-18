1392b672db9c77cc761e819dcf7b61984650c034
Dennis Xie <dennisxie1992@outlook.com>
2023-02-18 14:41:49 +0800

    beautify ctpsettle.py

--- tools/Settle/getsettle/ctpsettle.py
+++ tools/Settle/getsettle/ctpsettle.py
@@ -1,24 +1,30 @@
 """
 written by DennisXie on 2023-2-18
 """
-
+import sys
 import time
 import thosttraderapi as api
 
 
 class UserConfig(object):
-
-    borkerId: str = ""
+    brokerId: str = ""
     userId: str = ""
     password: str = ""
     appId: str = ""
     authCode: str = ""
 
+    def __init__(self, brokerId: str, userId: str, password: str, appId: str, authCode: str):
+        self.brokerId = brokerId
+        self.userId = userId
+        self.password = password
+        self.appId = appId
+        self.authCode = authCode
+
 
 class CTdClient(api.CThostFtdcTraderSpi):
-    def __init__(self, tdapi: api.CThostFtdcTraderApi, userConfig: UserConfig, front: str):
+    def __init__(self, userConfig: UserConfig, front: str):
         super().__init__()
-        self.tdapi: api.CThostFtdcTraderApi = tdapi
+        self.tdapi: api.CThostFtdcTraderApi = api.CThostFtdcTraderApi.CreateFtdcTraderApi(f"connect\\{userConfig.userId}")
         self.userConfig = userConfig
         self.front: str = front
         self.__reqId: int = 0
@@ -34,7 +40,7 @@ class CTdClient(api.CThostFtdcTraderSpi):
     def ready(self) -> bool:
         return self.__ready
 
-    def init(self):
+    def connect(self):
         self.tdapi.RegisterSpi(self)
         self.tdapi.SubscribePrivateTopic(api.THOST_TERT_QUICK)
         self.tdapi.SubscribePublicTopic(api.THOST_TERT_QUICK)
@@ -43,19 +49,14 @@ class CTdClient(api.CThostFtdcTraderSpi):
         while not self.__ready:
             time.sleep(0.2)
 
-    def querySettlementInfo(self, tradingDay: str, currencyId: str = "CNY"):
-        req = api.CThostFtdcQrySettlementInfoField()
-        req.BrokerID = self.userConfig.brokerId
-        # req.TradingDay = tradingDay
-        req.InvestorID = self.userConfig.userId
-        # req.AccountID = self.userConfig.userId
-        # req.CurrencyID = currencyId
-        self.tdapi.ReqQrySettlementInfo(req, self.reqId)
-
     def OnFrontConnected(self):
         """前置连接成功"""
         print("OnFrontConnected")
         self.authenticate()
+
+    def OnFrontDisconnected(self, nReason):
+        "前置断开连接"
+        print(f"Front disconnect, error_code={nReason}")
 
     def authenticate(self):
         req = api.CThostFtdcReqAuthenticateField()
@@ -68,11 +69,13 @@ class CTdClient(api.CThostFtdcTraderSpi):
     def OnRspAuthenticate(self, pRspAuthenticateField: api.CThostFtdcRspAuthenticateField,
                           pRspInfo: api.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
         """客户端认证响应"""
-        print(f"authenticate {pRspInfo}")
         if pRspInfo is not None:
             print(f"authenticate failed, ErrorID: {pRspInfo.ErrorID}, ErrorMsg: {pRspInfo.ErrorMsg}")
-        # else:
+
+        if pRspInfo is None or pRspInfo.ErrorID == 0:
             self.login()
+        else:
+            exit(1)
 
     def login(self):
         req = api.CThostFtdcReqUserLoginField()
@@ -87,25 +90,20 @@ class CTdClient(api.CThostFtdcTraderSpi):
         """登录响应"""
         if pRspInfo is not None:
             print(f"login failed, ErrorID: {pRspInfo.ErrorID}, ErrorMsg: {pRspInfo.ErrorMsg}")
-        # else:
-            print("login success")
-            # self.__ready = True
-            self.__today = pRspUserLogin.TradingDay
-            self.settlementConfirm()
 
-    def settlementConfirm(self):
-        req = api.CThostFtdcSettlementInfoConfirmField()
-        req.BrokerID = self.userConfig.borkerId
-        req.InvestorID = self.userConfig.userId
-        self.tdapi.ReqSettlementInfoConfirm(req, 0)
-        # self.tdapi.ReqSettlementInfoConfirm(req, self.reqId)
-
-    def OnRspSettlementInfoConfirm(self, pSettlementInfoConfirm, pRspInfo, nRequestID, bIsLast):
-        if pRspInfo is not None and pRspInfo.ErrorID != 0:
-            print(f"confirm failed, ErrorID: {pRspInfo.ErrorID}, ErrorMsg: {pRspInfo.ErrorMsg}")
-        else:
-            print("confirm success")
+        if pRspInfo is None or pRspInfo.ErrorID == 0:
             self.__ready = True
+            self.__today = pRspUserLogin.TradingDay
+        else:
+            exit(1)
+
+    def querySettlementInfo(self, tradingDay: str):
+        print(f"query settlement {self.userConfig}")
+        req = api.CThostFtdcQrySettlementInfoField()
+        req.BrokerID = self.userConfig.brokerId
+        req.TradingDay = tradingDay
+        req.InvestorID = self.userConfig.userId
+        self.tdapi.ReqQrySettlementInfo(req, self.reqId)
 
     def OnRspQrySettlementInfo(self, pSettlementInfo: api.CThostFtdcSettlementInfoField,
                                pRspInfo: api.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
@@ -115,8 +113,21 @@ class CTdClient(api.CThostFtdcTraderSpi):
         if pSettlementInfo is not None:
             print(pSettlementInfo.Content)
         else:
-            print(pSettlementInfo)
-            print("pSettlementInfo null")
+            print("empty settlement content")
+
+    def settlementConfirm(self):
+        # TODO: 是否要干掉confirm
+        req = api.CThostFtdcSettlementInfoConfirmField()
+        req.BrokerID = self.userConfig.brokerId
+        req.InvestorID = self.userConfig.userId
+        self.tdapi.ReqSettlementInfoConfirm(req, self.reqId)
+
+    def OnRspSettlementInfoConfirm(self, pSettlementInfoConfirm, pRspInfo, nRequestID, bIsLast):
+        if pRspInfo is not None:
+            print(f"confirm failed, ErrorID: {pRspInfo.ErrorID}, ErrorMsg: {pRspInfo.ErrorMsg}")
+        else:
+            print("confirm success")
+            self.__ready = True
 
     def OnRspQryInstrument(self, pInstrument: api.CThostFtdcInstrumentField,
                            pRspInfo: api.CThostFtdcRspInfoField, nRequestID: int, bIsLast: bool):
@@ -131,17 +142,11 @@ class CTdClient(api.CThostFtdcTraderSpi):
 
 if __name__ == "__main__":
     front = "tcp://180.168.146.187:10130"
-    user = UserConfig()
-    user.brokerId = "9999"
-    user.authCode = "0000000000000000"
-    user.appId = "simnow_client_test"
-    user.userId = "203199"
-    user.password = ""
-
-    tdapi = api.CThostFtdcTraderApi.CreateFtdcTraderApi(user.userId)
-    client = CTdClient(tdapi, user, front)
-    client.init()
-    client.querySettlementInfo("20230215")
+    # brokerId, userId, password, appId, authCode
+    user = UserConfig(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    client = CTdClient(user, front)
+    client.connect()
+    client.querySettlementInfo("20230217")
     i = 0
     while i < 10:
         time.sleep(1)
